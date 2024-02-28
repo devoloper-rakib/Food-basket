f154512698cb731ce9e27997a2491c57f5868a34
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-02-29 01:58:25 +0600

    layout and header added and implemented taildwind css class modifier

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+	return (
+		<div className='py-6 border-b-2 border-b-orange-500 '>
+			<div className='container flex items-center justify-between mx-auto'>
+				<Link
+					to='/'
+					className='text-3xl font-bold tracking-tight text-orange-500 '
+				>
+					Food-Basket
+				</Link>
+			</div>
+		</div>
+	);
+};
+
+export default Header;
