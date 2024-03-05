fe74e269682ece1d291493ad6b1188d1cfc1cd15
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-06 03:07:34 +0600

    pre populated the restaurant data

--- frontend/src/forms/manage-restaurant-form/ImageSection.tsx
+++ frontend/src/forms/manage-restaurant-form/ImageSection.tsx
@@ -7,9 +7,12 @@ import {
 	FormMessage,
 } from '../../components/ui/form';
 import { Input } from '../../components/ui/input';
+import { AspectRatio } from '../../components/ui/aspect-ratio';
 
 const ImageSection = () => {
-	const { control } = useFormContext();
+	const { control, watch } = useFormContext();
+
+	const existingImageUrl = watch('imageUrl');
 
 	return (
 		<div className='space-y-2'>
@@ -21,7 +24,16 @@ const ImageSection = () => {
 				</FormDescription>
 			</div>
 
-			<div className='flex flex-col gap-8 w-[50%]'>
+			<div className='flex flex-col gap-8 md:w-[50%]'>
+				{existingImageUrl && (
+					<AspectRatio ratio={16 / 9}>
+						<img
+							src={existingImageUrl}
+							alt={existingImageUrl.toString()}
+							className='rounded-md object-cover h-full w-full'
+						/>
+					</AspectRatio>
+				)}
 				<FormField
 					control={control}
 					name='imageFile'
