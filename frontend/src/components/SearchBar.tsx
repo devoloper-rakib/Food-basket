b02ba75ba7ae498f5ad8898d0e03228048331ce3
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-06 16:47:59 +0600

    frontend searchbar added

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,86 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Form, FormControl, FormField, FormItem } from './ui/form';
+import { Search } from 'lucide-react';
+import { Input } from './ui/input';
+import { Button } from './ui/button';
+
+const formSchema = z.object({
+	searchQuery: z.string({
+		required_error: 'Restaurant name is required',
+	}),
+});
+
+export type SearchForm = z.infer<typeof formSchema>;
+
+type Props = {
+	onSubmit: (formData: SearchForm) => void;
+	onReset?: () => void;
+	placeHolder: string;
+};
+
+const SearchBar = ({ onSubmit, onReset, placeHolder }: Props) => {
+	const form = useForm<SearchForm>({
+		resolver: zodResolver(formSchema),
+	});
+
+	const handleReset = () => {
+		form.reset({
+			searchQuery: '',
+		});
+
+		if (onReset) {
+			onReset();
+		}
+	};
+
+	return (
+		<Form {...form}>
+			<form
+				onSubmit={form.handleSubmit(onSubmit)}
+				className={`flex items-center flex-1 gap-3 justify-between flex-row border-2 rounded-full p-3 mx-5 ${
+					form.formState?.errors?.searchQuery && 'border-red-500'
+				}`}
+			>
+				<Search
+					strokeWidth={2.5}
+					size={30}
+					className='hidden ml-1 text-orange-500 md:block'
+				/>
+				<FormField
+					control={form.control}
+					name='searchQuery'
+					render={({ field }) => (
+						<FormItem className='flex-1'>
+							<FormControl>
+								<Input
+									{...field}
+									className='text-xl border-none shadow-none focus-visible:ring-0'
+									placeholder={placeHolder}
+								/>
+							</FormControl>
+						</FormItem>
+					)}
+				/>
+				{form.formState.isDirty && (
+					<Button
+						onClick={handleReset}
+						type='button'
+						variant='outline'
+						className='rounded-full'
+					>
+						Clear
+					</Button>
+				)}
+
+				<Button type='submit' className='bg-orange-500 rounded-full'>
+					Search
+				</Button>
+			</form>
+		</Form>
+	);
+};
+
+export default SearchBar;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,15 +1,30 @@
 import landingPage from '../assets/landing.png';
 import appDownloadImage from '../assets/appDownload.png';
+import SearchBar, { SearchForm } from '../components/SearchBar';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+	const navigate = useNavigate();
+
+	const handleSearchSubmit = (searchFormValues: SearchForm) => {
+		navigate({
+			pathname: `/search/${searchFormValues.searchQuery}`,
+		});
+	};
+
 	return (
-		<div className='flex flex-col gap-12'>
-			<div className='flex flex-col gap-5 py-8 -mt-16 text-center bg-white rounded-lg shadow-md'>
-				<h1 className='text-5xl font-bold tracking-tight text-orange-600'>
+		<div className='flex flex-col gap-12 '>
+			<div className='flex flex-col gap-5 py-8 -mt-16 text-center bg-white rounded-lg shadow-md md:px-32'>
+				<h1 className='px-2 text-2xl font-bold tracking-tight text-orange-600 md:text-4xl md:px-0'>
 					Embrace your inner couch potato and indulge in some sofa cuisine
 					tonight! 🛋️🍔🥡
 				</h1>
 				<span className='text-xl '>Food is just a click away!</span>
+
+				<SearchBar
+					placeHolder='Search by city or town'
+					onSubmit={handleSearchSubmit}
+				/>
 			</div>
 			<div className='grid gap-5 md:grid-cols-2'>
 				<img src={landingPage} alt='Landing page' />
