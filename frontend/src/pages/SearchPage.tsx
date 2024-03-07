5a8e14a92ee8216033ad687f04c0d5c5de42e36c
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-07 16:47:03 +0600

    pagination functionlist added

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -4,9 +4,11 @@ import SearchResultInfo from '../components/SearchResultInfo';
 import SearchResultCard from '../components/SearchResultCard';
 import { useState } from 'react';
 import SearchBar, { SearchForm } from '../components/SearchBar';
+import PaginationSelector from '../components/PaginationSelector';
 
 export type SearchState = {
 	searchQuery: string;
+	page: number;
 };
 
 // ERROR: in search options  first we need to reset all the search query fields when we search something
@@ -15,13 +17,22 @@ const SearchPage = () => {
 	const { city } = useParams();
 	const [searchState, setSearchState] = useState<SearchState>({
 		searchQuery: '',
+		page: 1,
 	});
 	const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+	const setPage = (page: number) => {
+		setSearchState((prevState) => ({
+			...prevState,
+			page,
+		}));
+	};
 
 	const setSearchQuery = (searchFormData: SearchForm) => {
 		setSearchState((prevState) => ({
 			...prevState,
 			searchQuery: searchFormData.searchQuery,
+			page: 1,
 		}));
 	};
 
@@ -29,6 +40,7 @@ const SearchPage = () => {
 		setSearchState((prevState) => ({
 			...prevState,
 			searchQuery: '',
+			page: 1,
 		}));
 	};
 
@@ -51,6 +63,12 @@ const SearchPage = () => {
 				{results?.data.map((restaurant, index) => (
 					<SearchResultCard key={index} restaurant={restaurant} />
 				))}
+
+				<PaginationSelector
+					page={results.pagination.page}
+					pages={results.pagination.pages}
+					onPageChange={setPage}
+				/>
 			</div>
 		</div>
 	);
